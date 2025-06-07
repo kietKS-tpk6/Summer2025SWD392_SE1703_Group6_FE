5e19867ce093f568774cc964e997941b8533d16f
Pham-Quy <quyphamphu03@gmail.com>
2025-06-07 10:14:30 +0700

    Feat : Add api to api.js

--- Hangul Learning System/src/config/api.js
+++ Hangul Learning System/src/config/api.js
@@ -1,9 +1,11 @@
-export const API_URL = 'http://localhost:5173/';
+export const API_URL = 'https://localhost:7201/';
 
 export const endpoints = {
     account: {
         login: 'api/Authentication/login',
         register: 'api/Authentication/register',
+        verifyOTP: 'api/Authentication/verify-otp',
+        sendOTP: 'api/Email/send'
     },
     manageAccount: {
         
